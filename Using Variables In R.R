107ec731b11795dee2a9919ff1feb4ff8a8bfcea
Nitin Akash <nitinakash@Nitins-MacBook-Air.local>
2018-01-14 19:54:23 -0400

    Using Variables in R

---
+++ Using Variables In R.R
@@ -0,0 +1,32 @@
+
+#Using Variable in R
+A <- 10
+B <- 5
+
+#Adding variable in R
+C <- A+B
+
+#Print value of variable C
+C
+
+#Another Example
+
+#Variable 1
+var1 <- 2.5
+
+#Variable 2
+var2 <- 4
+
+result <- var1 /var2
+result
+
+#Using function
+answer <- sqrt(var2)
+answer
+
+#Working with charecter Variable
+greeting  <- "Hello"
+name <- "Nitin"
+
+message <- paste(greeting, name)
+message
