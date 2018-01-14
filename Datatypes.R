d6396dfabe50bdffaef8cb691dbca56b89d3b339
Nitin Akash <nitinakash@Nitins-MacBook-Air.local>
2018-01-14 19:42:33 -0400

    Variables in R

---
+++ Datatypes.R
@@ -0,0 +1,26 @@
+
+#Datatypes in R
+#Atomic Datatype
+
+#Integer Datatype
+x <- 2L
+typeof(x)
+
+#Double Datatype
+y <- 2.5
+typeof(y)
+
+#Complex
+z <- 3+2i
+typeof(z)
+
+#Charecter
+a <- "h"
+typeof(a)
+
+#logical
+q1 <- T
+typeof(q1)
+
+q2 <- FALSE
+typeof(q2)
