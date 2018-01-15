6e32077e8ca9e9c641a5341d284aa1fdd70b6edf
Nitin Akash <nitinakash@Nitins-MacBook-Air.local>
2018-01-15 01:13:22 -0400

    Packages in R

---
+++ Packages in R.R
@@ -0,0 +1,13 @@
+#Packages in R
+
+
+#To install package in R
+install.packages("ggplot2")
+
+#To activate package use library function
+library(ggplot2)
+
+#Functions in ggplot package
+?qplot()
+?ggplot()
+?diamonds
