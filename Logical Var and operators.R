93d8baba30090f8cea4511311e8332cdb04a71b6
Nitin Akash <nitinakash@Nitins-MacBook-Air.local>
2018-01-14 20:02:47 -0400

    Logical variables and operators in R

---
+++ Logical Var and operators.R
@@ -0,0 +1,14 @@
+#Logical variables and operators
+
+#Logical operators
+#TRUE T
+#FALSE F
+
+4 < 5
+10 > 100
+4 == 5
+
+# ==, !=, <, >, >=, <=, !, |, &,  isTRUE(variable)
+result <- 4 < 5
+result
+typeof(result)
